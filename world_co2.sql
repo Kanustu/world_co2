9527de1fed2abfd6ab69de872b9d97228ff63f5e
JDKanius <kanustu@gmail.com>
2024-06-11 12:45:20 -0600

    finished sql file

--- world_co2.sql
+++ world_co2.sql
@@ -1,5 +1,6 @@
---World Co2 exploration
---Skills used: Joins, CTE's, Case Statements, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+/*World Co2 exploration
+
+Skills used: Joins, CTE's, Case Statements, Window Functions, Aggregate Functions, Creating Views, Converting Data Types*/
 
 CREATE Table country_info (
 	id SERIAL PRIMARY KEY,
@@ -20,116 +21,37 @@ CREATE TABLE co2 (
     share_global_co2 FLOAT,
     FOREIGN KEY (ci_id) REFERENCES country_info(id)
 );
-COMMENT ON COLUMN
 
---Looking at total co2 vs coal_co2
---Shows the percentage of co2 produced by coal within any given year and country
-SELECT ci.country, ci.year, co.co2, co.coal_co2, (co.coal_co2/co.co2)*100 AS coal_percentage
-FROM country_info ci
-JOIN co2 co
-ON ci.id = co.ci_id
-WHERE co2 IS NOT NULL
-    AND coal_co2 IS NOT NULL
-	AND co.co2 != 0
-	--add case statement to deal with zeroes
-	
---Looking at co2 per capita levels
-SELECT ci.country, 
+--Select and display all the data that will be used
+SELECT 
+    ci.country, 
     ci.year, 
 	ci.population, 
-	co.co2, 
-	ROUND(CAST(co.co2*1000000/ci.population AS numeric), 2) AS per_capita
+	ci.gdp, 
+	co.cement_mt, 
+	co.co2_mt, 
+	co.coal_mt, 
+	co.gas_mt, 
+	co.oil_mt
 FROM country_info ci
     JOIN co2 co
-    ON ci.id = co.ci_id
-WHERE co2 IS NOT NULL
-    AND population IS NOT NULL
-	AND year = 2020
-ORDER BY per_capita DESC
+    ON ci.id = co.ci_id;
 	
-	
--- Join country_info/co2 tables
-SELECT * FROM country_info AS ci
-JOIN co2 AS co
-ON ci.id = co.ci_id
 
--- Count entries of joined tables
-SELECT COUNT(*)
+-- Creating a filtered view of the combined data, including only current countries and no null co2 data
+CREATE VIEW current_countries AS
+SELECT 
+    ci.country, 
+	ci.year, 
+	ci.population, 
+	ci.gdp, 
+	co.cement_mt, 
+	co.co2_mt, 
+	co.coal_mt, 
+	co.gas_mt, 
+	co.oil_mt
 FROM country_info AS ci
 JOIN co2 AS co
-ON ci.id = co.ci_id
-
---Total co2 by year
-SELECT ci.year, SUM(co.co2) AS total_co2
-FROM country_info as ci
-JOIN co2 as co
-    ON ci.id = co.ci_id
-GROUP BY ci.year
-ORDER BY total_co2 DESC
-
-
---Calculate and display the percentage of coal, oil, gas and cement CO2 emissions in total CO2 emissions by year, 
---ordered by year
-SELECT ci.year, 
-    ROUND(CAST(SUM(co.co2) AS numeric), 2) AS total_co2, 
-    --ROUND(CAST(SUM(co.coal_co2) AS numeric), 2) AS total_coal,
-    ROUND(CAST(SUM(co.coal_co2)/SUM(co.co2) AS numeric) * 100, 2) AS coal_percentage,
-	ROUND(CAST(SUM(co.gas_co2)/SUM(co.co2) AS numeric) * 100, 2) AS gas_percentage,
-	ROUND(CAST(SUM(co.oil_co2)/SUM(co.co2) AS numeric) * 100, 2) AS oil_percentage,
-	ROUND(CAST(SUM(co.cement_co2)/SUM(co.co2) AS numeric) * 100, 2) AS cement_percentage
-FROM country_info as ci
-JOIN co2 as co
-    ON ci.id = co.ci_id
-GROUP BY ci.year
-ORDER BY ci.year
-
-
---Calculate and display the total historic co2 emissions and the percentage of that total ordered by top 10 countries
-SELECT ci.country, 
-    ROUND(CAST(SUM(co.co2) AS numeric), 2) AS cumlative_co2
-FROM country_info AS ci
-JOIN co2 AS co
-    ON ci.id = co.ci_id
-WHERE country != 'World' 
-    AND country NOT LIKE '%countries' 
-    AND country NOT LIKE '%(%' 
-    AND country NOT IN ('Asia', 'Europe', 'North America', 'Africa', 'South America')
-GROUP BY country
-HAVING SUM(co.co2) NOTNULL
-ORDER BY cumlative_co2 DESC
-limit 10;
--- Creating a filtered view of the combined data, including only countries and no null co2 data
-CREATE VIEW country_data AS
-SELECT * FROM country_info AS ci
-JOIN co2 AS co
-ON ci.id = co.ci_id
-WHERE co2 IS NOT NULL
-    OR cement_co2 IS NOT NULL
-    OR coal_co2 IS NOT NULL
-	OR gas_co2 IS NOT NULL
-	OR oil_co2 IS NOT NULL 
-	
-DROP VIEW country_data
---percent total with nested queries??
-WITH total_co2 AS (
-    SELECT SUM(CAST(co.co2 AS numeric)) AS total_co2
-    FROM country_info AS ci
-    JOIN co2 AS co
-    ON ci.id = co.ci_id
-)
-
-SELECT ci.country, SUM(co.co2) FROM country_info AS ci
-JOIN co2 AS co
-ON ci.id = co.ci_id
-GROUP BY country
-HAVING SUM(co.co2) NOTNULL
-ORDER BY SUM(co.co2) DESC
-
---Select and display total co2 of highest 10 countries, broken down by coal, gas, oil, and cement co2 for a specific year.
-
-SELECT ci.country, co.co2, co.coal_co2, co.gas_co2, co.oil_co, co.cement_co2
-FROM country_info AS ci
-    JOIN co2 AS co
     ON ci.id = co.ci_id
 WHERE country NOT IN('World', 'Africa', 'South America') 
     AND country NOT LIKE '%countries%'
@@ -138,33 +60,141 @@ WHERE country NOT IN('World', 'Africa', 'South America')
 	AND country NOT LIKE '%Asia%'
 	AND country NOT LIKE '%Europe%'
 	AND country NOT LIKE 'North America%'
-    AND year = 1999
-	AND co.co2 IS NOT NULL
-	AND co.coal_co2 IS NOT NULL
-	AND co.gas_co2 IS NOT NULL
-	AND co.oil_co IS NOT NULL
-	AND co.cement_co2 IS NOT NULL
-ORDER BY co.oil_co DESC
+	AND co.co2_mt IS NOT NULL
+	AND co.coal_mt IS NOT NULL
+	AND co.gas_mt IS NOT NULL
+	AND co.oil_mt IS NOT NULL
+	AND co.cement_mt IS NOT NULL;
+
+
+--Select and display total cumulative co2 levels
+SELECT 
+    country, 
+	ROUND(SUM(co2_mt)) as cumulative_co2
+FROM current_countries
+GROUP BY country
+HAVING SUM(co2_mt) IS NOT NULL
+ORDER BY cumulative_co2 DESC
 LIMIT 10;
 
---Select and display c
-SELECT *
-FROM country_info AS ci
-JOIN co2 AS co
-ON ci.id = co.ci_id
-WHERE year = 2021
-    AND population IS NOT NULL
-	AND country NOT LIKE '%countries%'
-    AND country NOT LIKE '%GCP%'
-	AND country NOT LIKE '%International%'
-	AND country NOT LIKE '%Asia%'
-	AND country NOT LIKE '%Europe%'
-	AND country NOT LIKE 'North America%'
-	AND country NOT IN('World', 'Africa', 'South America')
-	AND co.co2 IS NOT NULL
-	AND co.coal_co2 IS NOT NULL
-	AND co.gas_co2 IS NOT NULL
-	AND co.oil_co IS NOT NULL
-	AND co.cement_co2 IS NOT NULL
+
+--SELECT and display co2 per capita levels for Canada in metric tonnes
+SELECT 
+    country, 
+	year, 
+	ROUND(CAST(co2_mt/population*1000000 AS numeric),2) AS per_capita_co2
+FROM current_countries
+WHERE country = 'Canada';
 
 
+--Calculate and display percentage change in per capita year over year
+WITH per_capita AS(
+SELECT 
+    country, 
+	year, 
+	ROUND(CAST(co2_mt/population*1000000 AS numeric),2) AS per_capita_co2
+FROM current_countries
+)
+, per_capita_lag AS (
+SELECT 
+	country, 
+	year, 
+	per_capita_co2,
+	LAG(per_capita_co2) OVER (PARTITION BY country ORDER BY year) AS prev_per_capita
+FROM per_capita
+)
+SELECT 
+    country, 
+	year, 
+	per_capita_co2, 
+	CASE
+	    WHEN prev_per_capita IS NULL THEN NULL
+		ELSE ROUND((per_capita_co2 - prev_per_capita) / prev_per_capita * 100)
+	END AS pct_change
+FROM per_capita_lag
+WHERE year = 2020
+ORDER BY pct_change;
+
+
+/*Calculate and display the percentage of coal, oil, gas and cement CO2 emissions
+in total cumulative CO2 emissions by year*/
+SELECT 
+    year, 
+    ROUND(CAST(SUM(co2_mt) AS numeric), 2) AS total_co2, 
+    ROUND(CAST(SUM(coal_mt)/SUM(co2_mt) AS numeric) * 100, 2) AS coal_percentage,
+	ROUND(CAST(SUM(gas_mt)/SUM(co2_mt) AS numeric) * 100, 2) AS gas_percentage,
+	ROUND(CAST(SUM(oil_mt)/SUM(co2_mt) AS numeric) * 100, 2) AS oil_percentage,
+	ROUND(CAST(SUM(cement_mt)/SUM(co2_mt) AS numeric) * 100, 2) AS cement_percentage
+FROM current_countries
+GROUP BY year
+ORDER BY year;
+
+
+/*Calculate and display the percentage of coal, oil, gas and cement CO2 emissions
+in total cumulative CO2 emissions by country*/
+SELECT 
+    country,
+	ROUND(CAST(SUM(co2_mt) AS numeric), 2) AS total_co2, 
+    ROUND(CAST(SUM(coal_mt)/SUM(co2_mt) AS numeric) * 100, 2) AS coal_percentage,
+	ROUND(CAST(SUM(gas_mt)/SUM(co2_mt) AS numeric) * 100, 2) AS gas_percentage,
+	ROUND(CAST(SUM(oil_mt)/SUM(co2_mt) AS numeric) * 100, 2) AS oil_percentage,
+	ROUND(CAST(SUM(cement_mt)/SUM(co2_mt) AS numeric) * 100, 2) AS cement_percentage
+FROM current_countries
+GROUP BY country
+ORDER BY total_co2 DESC;
+ 
+
+--Calculate and display the total historic co2 emissions and the percentage of that total ordered by top 10 countries
+WITH total_co2 AS(
+SELECT 
+	ROUND(SUM(co2_mt)*1000000) AS total_co2
+FROM current_countries
+)
+
+SELECT 
+    country, 
+	ROUND(SUM(cc.co2_mt)) AS total_co2_mt,
+    ROUND(SUM(cc.co2_mt*1000000/tc.total_co2 * 100)) AS percentage
+FROM current_countries cc
+CROSS JOIN total_co2 tc
+GROUP BY country
+ORDER BY percentage DESC
+LIMIT 10;
+
+
+--Calculate and display year over year percentage change
+WITH lag_data AS (
+SELECT 
+    year, 
+	country,
+	gdp,
+	population,
+	co2_mt,
+	LAG(gdp) OVER (PARTITION BY country ORDER BY year) AS prev_gdp,
+	LAG(co2_mt) OVER (PARTITION BY country ORDER BY year) AS prev_co2,
+	LAG(population) OVER (PARTITION BY country ORDER BY year) AS prev_pop
+FROM current_countries
+)
+SELECT 
+    year, 
+	country,
+	gdp,
+	CASE
+	    WHEN prev_gdp != 0
+		THEN ROUND((gdp - prev_gdp)/prev_gdp * 100)
+		ELSE NULL
+	END AS gdp_pct_change,
+	co2_mt,
+	CASE
+	    WHEN prev_co2 != 0
+		THEN ROUND((co2_mt - prev_co2)/prev_co2 * 100)
+		ELSE NULL
+	END AS co2_pct_change,
+	population,
+	CASE
+	    WHEN prev_pop != 0
+		THEN ROUND((population - prev_pop)/prev_pop * 100)
+		ELSE NULL
+	END AS pop_pct_change
+FROM lag_data
+WHERE country = 'Canada';
